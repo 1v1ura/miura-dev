979c1b10bc2e119bb802d4089f61ad51df0a2725
Takuma Miura <21156330+tkm-mur@users.noreply.github.com>
2021-02-27 21:19:10 +0900

    feat: append console

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import '@/styles/index.css'
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    console.log(
+      '%cWelcome to Miura.dev 🎉',
+      'color: green; font-size: 20px; font-weight: bold;'
+    )
+  }, [])
+
   return <Component {...pageProps} />
 }
